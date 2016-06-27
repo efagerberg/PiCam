4adf12986697a2d4e75bbedb5cbc38ecb82619e2
Evan Fagerberg <efagerberg@safaribooksonline.com>
2016-06-27 10:54:42 -0400

    Updating main file with motion sensor and email logic.

--- security_camera.py
+++ security_camera.py
@@ -1,6 +1,11 @@
 import picamera
 import time
 from RPi import GPIO
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+from gpiozero import MotionSensor
+from datetime import datetime
 
 
 def provision_pi_camera(hflip=False, vflip=False, zoom=(0.0, 0.0, 1.0, 1.0), video_stabilization=True):
@@ -12,12 +17,8 @@ def provision_pi_camera(hflip=False, vflip=False, zoom=(0.0, 0.0, 1.0, 1.0), vid
     return camera
 
 
-def initialize_gpio_pins():
-    print("This is where we set our input pins and output pins for LEDs, motion sensor, and servo")
-
-
 def rotate_servo(angle):
-    print("This is where we would rotate the servo {} degrees.".format(angle))
+    print("This is where we would rotate the servo to {} degrees.".format(angle))
 
 
 def reset_servo():
@@ -28,11 +29,37 @@ def turn_on_flood_light():
     print("This is where we would turn the LED array on.")
 
 
+def send_email():
+    # This function will need to take in pictures and add them as attachments.
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("SpyPi@no-reply")
+    subject = "Motion Detected"
+    to_email = Email("sendToUser@example.com")
+    content = Content("text/plain", "We have detected motion from your pi!\n\n")
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+
 def main():
-    camera = provision_pi_camera()
-    camera.start_recording("test.h264")
-    camera.wait_recording(10)
-    camera.stop_recording()
+    pir = MotionSensor(4)
+    with provision_pi_camera() as camera:
+        while True:
+            if pir.motion_detected:
+                now = datetime.now()
+                camera.capture("{}-initial.jpg".format(now.iso_format()))
+                turn_on_flood_light()
+                rotate_servo(0)
+                camera.capture("{}-left.jpg".format(now.iso_format()))
+                rotate_servo(90)
+                camera.capture("{}-middle.jpg".format(now.iso_format()))
+                rotate_servo(180)
+                camera.capture("{}-right.jpg".format(now.iso_format()))
+                reset_servo()
+                send_email()
+
 
 if __name__ == "__main__":
     main()
