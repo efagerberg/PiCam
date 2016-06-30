19e4d50e0dc2719a29faa334449d717e5011e0a7
Evan Fagerberg <adioevan@gmail.com>
2016-06-30 01:16:37 +0000

    Updating install script

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ A python3 file that controls a Raspberry Pi camera, LEDs, and motion sensor.
 
 # Install MotionEye
 * Refer to `https://github.com/ccrisan/motioneye/wiki/Install-On-Raspbian`
-* `./install.sh`
+* `sudo ./install.sh`
 * Move init.sh to init folder
 
 # Update motioneye
-* `./upgrade.sh`
+* `sudo ./upgrade.sh`
 
 # Configure Sendgrid
 Make an account and generate a free API Key
@@ -18,7 +18,3 @@ Configure your enviornment
 * `echo "sendgrid.env" >> .gitignore`
 * `source ./sendgrid.env`
 
-Install sendgrid
-* `sudo pip install sendgrid`
-
-Change the to_email value in "send_email.py" to send the email to yourself for testing purposes.
--- install.sh
+++ install.sh
@@ -1,15 +1,37 @@
 #!/bin/sh
+echo "Updating and installing python libraries."
 apt-get update
-sudo apt-get install python3-picamera
+apt-get install python3-picamera
+pip install sendgrid
+
+echo "Downloading and installing prebuilt ffmpeg."
+wget https://github.com/ccrisan/motioneye/wiki/precompiled/ffmpeg_2.8.3.git325b593-1_armhf.deb
+dpkg -i ffmpeg_2.8.3.git325b593-1_armhf.deb
+
+echo "Removing previous ffmpeg."
 apt-get remove libavcodec-extra-56 libavformat56 libavresample2 libavutil54
 apt-get install libavutil54 libavformat56 libswscale3
+echo "Downloading Mr Dave's version of motion."
+wget https://github.com/ccrisan/motioneye/wiki/precompiled/motion-mrdave-raspbian -O /usr/local/bin/motion
+chmod +x /usr/local/bin/motion
 
-apt-get install motion
+echo "Installing the dependencies."
 apt-get install python-pip python-dev curl libssl-dev libcurl4-openssl-dev libjpeg-dev
-
 pip install motioneye
 
+echo "Preparing the configuration directory."
 mkdir -p /etc/motioneye
 cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
 
+echo "Preparing the media directory."
 mkdir -p /var/lib/motioneye
+
+echo "Make motioneye start on boot."
+cp init-motion-server.sh /etc/init.d/init-motion-server.sh
+sudo update-rc.d init-motion-server.sh defaults
+
+echo "Allowing use of CSI camera module."
+if !grep -Fxq "bcm2835-v4l2" /etc/modules
+then
+	echo 'bcm2835-v4l2' >> /etc/modules
+fi
