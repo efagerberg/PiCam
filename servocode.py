ece163040c0285df2926cd4fba275bde92eda19e
Brian Ford <fordb1@wit.edu>
2016-07-05 19:13:15 -0400

    Paramaterized and got rid of input

--- servocode.py
+++ servocode.py
@@ -1,16 +1,14 @@
 import RPi.GPIO as GPIO
-servoPin=11
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servoPin, GPIO.OUT)
+def rotate_servo(angle):
+	servoPin = 18
 
-pwm=GPIO.PWM(servoPin, 50)
-pwm.start(7)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(servoPin, GPIO.OUT)
 
-for i in range(0,20)
-	desriedPosition=input("Where do you want the servo? 0-180")
-	DC=1./18.*(desiredPosition)+2
-	pwm.ChangeDutyCycle(DC)
-	
-pwm.stop()
-GPIO.cleanup()
+	#pwm object on servo pin with 50Hz signal
+    pwm = GPIO.PWM(servoPin, 50)
+    pwm.start(7)
+
+    DC = float(1 / 18) * (angle) + 2
+    pwm.ChangeDutyCycle(DC)
