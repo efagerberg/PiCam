b0b90fb064e5276b35b81cea4d56ff211d21b0e2
Justin Gagne <gagnej3@wit.edu>
2016-07-05 16:51:53 -0400

    Updated rotation

--- servocode.py
+++ servocode.py
@@ -1,14 +1,27 @@
 import RPi.GPIO as GPIO
+import time
 
-def rotate_servo(angle):
-	servoPin = 18
+servoPin=13
 
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(servoPin, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(servoPin, GPIO.OUT)
 
-	#pwm object on servo pin with 50Hz signal
-    pwm = GPIO.PWM(servoPin, 50)
-    pwm.start(7)
+#pwm object on servo pin with 50Hz signa
+pwm = GPIO.PWM(servoPin, 50)
+pwm.start(7)
 
-    DC = float(1 / 18) * (angle) + 2
-    pwm.ChangeDutyCycle(DC)
+try:
+	while True:
+		pwm.ChangeDutyCycle(7) #turn towards 90 degree
+		time.sleep(1)		 #sleep 1 second 
+		pwm.ChangeDutyCycle(2) #Turn toward 0 degree
+		time.sleep(1)		 #sleep 1 second
+		pwm.ChangeDutyCycle(7)
+		time.sleep(1)
+		pwm.ChangeDutyCycle(12)#turn toward 180 degree
+		time.sleep(1)		 # sleep 1 second 
+
+except KeyboardInterrupt:
+	pwm.stop()
+	GPIO.cleanup()
+
